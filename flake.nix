76dabdca6041771d876bcc88b89feb1a3cb26fd3
StepBroBD <Hi@StepBroBD.com>
2023-06-22 14:52:04 -0600

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs/master";
+
+    utils.url = "flake:flake-utils";
+
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , gomod2nix
+    , ...
+    }: utils.lib.eachSystem [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+    ]
+      (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            gomod2nix.overlays.default
+          ];
+        };
+
+        lib = pkgs.lib;
+
+        stdenv = pkgs.stdenv;
+
+        hopper = pkgs.buildGoApplication rec {
+          name = "hopper";
+
+          src = lib.cleanSource ./.;
+
+          modules = ./gomod2nix.toml;
+        };
+      in
+      {
+        packages.default = hopper;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gomod2nix.packages.${system}.default
+
+            llvmPackages.clang
+            llvmPackages.bintools
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+      );
+}
