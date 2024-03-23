f60beace8b9fbfacadba8b5508aabc6cc6dcb004
Luciano Remes <38026312+Cybergenik@users.noreply.github.com>
2024-03-23 19:21:05 -0400

    Add proper --help for node and master (#12)
    * add proper --help for node and master
    
    * fix logic in node help

--- cmd/hopper-node/main.go
+++ cmd/hopper-node/main.go
@@ -11,9 +11,49 @@ import (
 	n "github.com/Cybergenik/hopper/node"
 )
 
+func printHelp() {
+	fmt.Printf(
+		`NAME:
+    Hopper: Fuzzing Node
+
+SYNOPSIS: 
+    hopper-node [OPTIONS]... [--stdin|--args] -I <id> -T <path to target>
+
+DESCRIPTION:
+    Fuzzing Node, runs PUT and reports to Hopper Master
+
+    -I 
+        node ID, must be a unique unsigned integer
+    -T
+        path to instrumented target binary
+    -M
+        IP/address of Master, defaults to localhost
+    -P
+        port of Master, defaults to 6969
+    --raw 
+        feed raw seed bytes directly into the PUT, defaults to false. Hopper will put bytes in a file and feed that file to target
+    --args 
+        args to use against target, ex: --depth=1 @@
+    --env 
+        env variables for target seperated by a ';' ex: ENV1=foo;ENV2=bar;
+    --stdin 
+        feed seed through stdin, instead of as argument
+    --help 
+        prints this message
+
+EXAMPLES:
+    hopper-node -I 1 -T target --args "--depth=2 @@"
+        runs fuzzing node with id 1, target binary "target", with PUT arg "--depth=2", and it will replace "@@" with a file name of the seed
+
+    hopper-node -I 22 -T /home/user/trash/emacs --stdin
+        runs fuzzing node with id 22, target binary "/home/user/trash/emacs", and will feed the seed directly through stdin
+`)
+}
+
 func main() {
-	id := flag.Uint64("I", 0, "Node ID, usually just a unique int")
-	target := flag.String("T", "", "instrumented target binary")
+	help := flag.Bool("help", false, "help menu")
+	id := flag.Uint64("I", 0, "Node ID, must be a unique unsigned integer")
+	target := flag.String("T", "", "path to instrumented target binary")
 	args := flag.String("args", "", "args to use against target, ex: --depth=1 @@")
 	raw := flag.Bool("raw", false, "should input be fed as pure string (default: input as a file arg)")
 	env := flag.String("env", "", "env variables for target seperated by a `;`, ex: ARG1=foo;ARG2=bar;")
@@ -22,15 +62,17 @@ func main() {
 	port := flag.Int("P", 6969, "Port of Master")
 
 	flag.Parse()
-	err := ""
+	if *help {
+		printHelp()
+		os.Exit(0)
+	}
+	var err string
 	if *id == 0 {
-		err += "Hopper Node: Provide a unique Node Id greater than 0: -I \n"
-	}
-	if *target == "" {
-		err += "Hopper Node: Provide a command to run the target: -C\n"
-	}
-	if !strings.Contains(*args, "@@") && !*stdin {
-		err += "Hopper Node: Must provide an @@ input in args if not using stdin: ex --stdin or --args @@\n"
+		err = "Hopper Node: Provide a unique Node Id greater than 0: -I\n"
+	} else if *target == "" {
+		err = "Hopper Node: Provide a command to run the target: -C\n"
+	} else if !strings.Contains(*args, "@@") && !*stdin {
+		err = "Hopper Node: Must provide an @@ input in args if not using stdin: ex --stdin or --args @@\n"
 	}
 	if err != "" {
 		err += "--help | -h : to show help menu"
