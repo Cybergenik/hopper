c394618ddfd246b80b69310708d0d80de6561c0a
Luciano Remes <38026312+Cybergenik@users.noreply.github.com>
2024-03-31 03:09:28 -0400

    add readelf containerized demo to README and do some renaming (#16)

--- examples/binutils/readelf/master_docker.sh
+++ examples/binutils/readelf/master_docker.sh
@@ -4,20 +4,27 @@
 # Detach: Ctrl-a Ctrl-d
 # Attach: screen -r <pid>.master
 
-# Logging:
-export HOPPER_LOG=1
-export HOPPER_LOG_INTERVAL=10
+## Logger:
+#export HOPPER_LOG=1
+#export HOPPER_LOG_INTERVAL=10
 
-# Spawn Master
+## Create Hopper subnet
+docker network create hopper-subnet &> /dev/null
+
+## Spawn Master
 export HOPPER_OUT="/hopper_out"
 
-screen -S master -dm docker run -it --rm \
+docker run -it --rm \
     --name hopper-master \
     --env TERM \
     --env HOPPER_OUT \
     --env HOPPER_LOG \
     --env HOPPER_LOG_INTERVAL \
     --volume $(pwd)$HOPPER_OUT:$HOPPER_OUT \
+    --network hopper-subnet \
     --publish 6969:6969 \
     hopper-readelf:latest \
     bash -c "cd hopper && ./hopper-master -I ./examples/binutils/readelf/in -H=20"
+
+## Clean up subnet
+docker network rm hopper-subnet &> /dev/null
