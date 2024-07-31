d969481cb3d758ddf2398d61312820c75047241a
cybergenik <cybergenik@gmail.com>
2024-07-30 21:53:13 -0400

    adding cloudlab dist scripts

---
+++ examples/binutils/readelf/master_docker_dist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# List: screen -list
+# Detach: Ctrl-a Ctrl-d
+# Attach: screen -r <pid>.master
+
+# Logging:
+export HOPPER_LOG=1
+export HOPPER_LOG_INTERVAL=10
+
+# Spawn Master
+export HOPPER_OUT="/hopper_out"
+LOCAL_OUT="/proj/hopper-tests-PG0/defcon"
+
+screen -S master -dm docker run -it --rm \
+    --name hopper-master \
+    --env TERM \
+    --env HOPPER_OUT \
+    --env HOPPER_LOG \
+    --env HOPPER_LOG_INTERVAL \
+    --volume $LOCAL_OUT:$HOPPER_OUT \
+    --publish 6969:6969 \
+    hopper-readelf:latest \
+    bash -c "cd hopper && ./hopper-master -I ./examples/binutils/readelf/in -H=20"
+
---
+++ examples/binutils/readelf/node_docker_dist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export HOPPER_OUT="/hopper_out"
+LOCAL_OUT="/proj/hopper-tests-PG0/defcon"
+MASTER_IP="10.10.1.1"
+PORT="6969"
+
+## Spawn Nodes
+for ((i=$1;i<=$2;i++))
+do
+    nohup docker run --rm \
+        --name hopper-node$i \
+        --env TERM \
+        --env HOPPER_OUT \
+        --volume $(pwd)$HOPPER_OUT:$HOPPER_OUT \
+        hopper-readelf:latest \
+        bash -c "
+            cd /hopper;
+            ./hopper-node -I $i -T ./readelf_target -M $MASTER_IP -P $PORT --args '-a @@'" &> /dev/null &
+done
